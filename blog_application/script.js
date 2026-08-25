9509f9fe174bb68d8dfeb485568234ff05a96d1f
deepika <deepikarao63611@gmail.com>
2026-08-25 21:32:34 +0530

    frontend api fetched

--- blog_application/script.js
+++ blog_application/script.js
@@ -1,10 +1,32 @@
 
 //register form
-let form= document.querySelector("#RegisterForm")
+let form= document.getElementById("RegisterForm")
 if (form){
-form.addEventListener("submit",function(event){
+form.addEventListener("submit", async  function(event){
     event.preventDefault();
-    alert("Registration successful!")
+    let name=document.getElementById("name").value
+    let email=document.getElementById("email").value
+    let password=document.getElementById("password").value
+    let confirmPassword=document.getElementById("confirmPassword").value
+    if(password!==confirmPassword){
+        alert("passwords doesnot match")
+        return;
+    }
+    let response=await fetch("http://localhost:3000/register",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            name:name,
+            email:email,
+            password:password
+        })
+    })
+    let data =await response.json();
+    alert(data.message)
+    
+    //alert("Registration successful!")
     location.reload()
 
 })
@@ -12,21 +34,50 @@ form.addEventListener("submit",function(event){
 
 
 //login form
-let loginform=document.querySelector("#loginForm")
+let loginform=document.getElementById("loginForm")
 if (loginform){
-loginform.addEventListener("submit",function(event){
+loginform.addEventListener("submit", async function(event){
     event.preventDefault()
-    alert("Login successful")
+    let email=document.getElementById("email").value
+    let password=document.getElementById("password").value
+    let response=await fetch("http://localhost:3000/login",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            email:email,
+            password:password
+        })
+    })
+    let data=await response.json()
+    alert(data.message)
+    //alert("Login successful")
     location.reload()
 })
 }
 
 //create blog
-let blogform=document.querySelector("#blogForm")
+let blogform=document.getElementById("blogForm")
 if (blogform){
-    blogform.addEventListener("submit",function(event){
-        event.preventDefault()
-        alert("blog published successfully !")
+    blogform.addEventListener("submit", async function(event){
+        event.preventDefault();
+        let title=document.getElementById("title").value;
+        let category=document.getElementById("category").value;
+        let content=document.getElementById("text-area").value;
+        let image=document.getElementById("image").files[0];
+        let formData=new FormData();
+        formData.append("title",title);
+        formData.append("category",category)
+        formData.append("content",content)
+        formData.append("image",image)
+        let response=await fetch("http://localhost:3000/blogs",{
+            method:'POST',
+            body:formData
+        })
+        let data=await response.json()
+        alert(data.message)
+        //alert("blog published successfully !")
         location.reload()
 
     })
@@ -35,6 +86,8 @@ if (blogform){
 //hamburger
 const hamburger=document.getElementById("hamburger")
 const navLinks=document.getElementById("navLinks")
+if(hamburger){
 hamburger.addEventListener("click",function(){
     navLinks.classList.toggle("active")
 })
+}
